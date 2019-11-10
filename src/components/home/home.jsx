d472f77495305ecf27cdde9b2783d3e75d8e988e
m019m1 <50258878+m019m1@users.noreply.github.com>
2019-11-10 22:17:49 +0700

    changed images

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './home.css';
-import myPhoto from '../../img/17_3-min.jpg';
+import myPhoto from '../../img/me.jpg';
 
 class Home extends Component {
 
